12292193f4153eb9a726edcf4af582dd9ccc0534
selaguardia <selaguardia91@gmail.com>
2021-11-25 13:19:39 -0800

    finished app

--- src/App.js
+++ src/App.js
@@ -1,66 +1,107 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Tasks from './components/Tasks'
-import AddTask from './components/AddTask'
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
 
 function App() {
-  const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Vet Appointment for Eddie",
-      day: "Nov 29th at 3:00pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting on Zoom",
-      day: "Dec 1st at 8:00am",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Behavioral Interview w/ Apple",
-      day: "Dec 7th at 9:00am",
-      reminder: false,
-    },
-  ])
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+
+    getTasks();
+  }, []);
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+
+    return data;
+  };
+
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+
+    return data;
+  };
+
   // Add task
-  const addTask = (task) => {
-    const id= Math.floor(Math.random() *10000)+1
-    const newTask = { id, ...task} 
-    setTasks([...tasks, newTask])
-  }
+  const addTask = async (task) => {
+    const res = await fetch(`http://localhost:5000/tasks`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+    setTasks([...tasks, data]);
+    // const id= Math.floor(Math.random() *10000)+1
+    // const newTask = { id, ...task}
+    // setTasks([...tasks, newTask])
+  };
 
   // Delete tasks
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
-  }
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: "DELETE" });
+
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
   // Toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+
+    const data = await res.json()
+
     setTasks(
-      tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder } : task)
-    )
-  }
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    );
+  };
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length === 0 ? 'No tasks' : <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />}
-    </div>
+      <div className="container">
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+        {showAddTask && <AddTask onAdd={addTask} />}
+        {tasks.length === 0 ? (
+          "No tasks. Click add button."
+        ) : (
+          <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+        )}
+        <Footer />
+      </div>
   );
 }
 
 export default App;
 
-// {showAddTask && <AddTask onAdd={addTask} />} 
+// {showAddTask && <AddTask onAdd={addTask} />}
 // ... same thing as ...
 // {showAddTask ? <AddTask onAdd={addTask} : '' }
 
-
-// If you use class based components then you need to... 
+// If you use class based components then you need to...
 // import React from 'react';
 // import Header from './components/Header'
 
@@ -68,11 +109,11 @@ export default App;
 //   render() {
 //     return (
 //       <>
-//         <Header /> 
+//         <Header />
 //         <h1>Hello from a class component!</h1>
 //       </>
 //     )
-//   }  
+//   }
 // }
 
 // export default App
